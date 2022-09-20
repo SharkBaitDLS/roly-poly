dbd4d3bb5a9e5e6f3bd1b4da2f1170a3eaa48fcd
Isaac M. Good <imgood@imgoodproductions.org>
2022-09-20 00:22:33 -0700

    Formalize poisoned DB lock as unrecoverable

--- src/database.rs
+++ src/database.rs
@@ -7,15 +7,15 @@ use serenity::model::prelude::GuildId;
 use crate::guild_data::GuildData;
 
 pub fn get_guild_data(db: &RwLock<PickleDb>, guild_id: &GuildId) -> Option<GuildData> {
-    // TODO: decide how to handle poisoned lock
-    db.read().unwrap().get::<GuildData>(&guild_id.to_string())
+    db.read()
+        .expect("The database lock is poisoned due to a panic on write")
+        .get::<GuildData>(&guild_id.to_string())
 }
 
 pub fn update_guild_data(db: &RwLock<PickleDb>, guild_id: &GuildId, new_data: &GuildData) {
-    // TODO: decide how to handle poisoned lock
     if let Err(e) = db
         .write()
-        .unwrap()
+        .expect("The database lock is poisoned due to a panic on write")
         .set::<GuildData>(&guild_id.to_string(), new_data)
     {
         error!(
